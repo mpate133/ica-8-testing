6678aee233faa3a854a5cfbbf17ff0cabb8cc769
mpate133 <mpate133@asu.edu>
2022-10-27 16:50:57 -0700

    added codes in methods

--- Project/src/main/java/org/example/operationsForUrinals.java
+++ Project/src/main/java/org/example/operationsForUrinals.java
@@ -1,22 +1,44 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class operationsForUrinals {
 
-    public void getString(){
-
+    static Scanner sc = new Scanner(System.in);
+    public static void getString(int choice) throws FileNotFoundException {
+        if(choice == 0){
+            System.out.println("Enter  the string : ");
+            String str = sc.next();
+            if(isValidString(str)){
+                countMaximumAvailableUrinals(str);
+            }
+            else{
+                System.out.println("Invalid string entered");
+            }
+        }else{
+            readFromFile();
+        }
     }
 
-    public boolean isValidString(String str){
+    public static boolean isValidString(String str){
         boolean isValidInput = true;
+        char[] chars = str.toCharArray();
         System.out.println ("Not yet implemented");
         return isValidInput;
     }
 
-    public void readFromFile(){
+    public static void readFromFile() throws FileNotFoundException {
+        File file = new File("Project/src/main/resources/urinal.dat");
+        Scanner sc = new Scanner(file);
 
+        while (sc.hasNextLine()) {
+            countMaximumAvailableUrinals(sc.nextLine());
+        }
     }
 
-    public void countMaximumAvailableUrinals(String str){
+    public static void countMaximumAvailableUrinals(String str){
 
     }
 }
--- Project/src/main/java/org/example/urinals.java
+++ Project/src/main/java/org/example/urinals.java
@@ -1,11 +1,29 @@
 package org.example;
 
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class urinals {
 
     /*
     Author - Manan Hasmukhkumar Patel (mpate133)
      */
-    public static void main(String args[]){
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please select your choice:");
+        System.out.println("0 for Keyboard Input");
+        System.out.println("1 for File Input");
+
+        int choice = sc.nextInt();
+
+        if(choice == 1 || choice == 0){
+            operationsForUrinals.getString(choice);
+        }
+        else{
+            System.out.println("Code ended due to invalid choice");
+        }
 
     }
 }
