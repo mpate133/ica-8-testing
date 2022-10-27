7eea3f4cc72bdcea881c6341581ed7f23e9f571f
mpate133 <mpate133@asu.edu>
2022-10-27 15:32:05 -0700

    commit for skeleton

---
+++ Project/src/main/java/org/example/operationsForUrinals.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class operationsForUrinals {
+
+    public void getString(){
+
+    }
+
+    public boolean isValidString(String str){
+        boolean isValidInput = true;
+        System.out.println ("Not yet implemented");
+        return isValidInput;
+    }
+
+    public void readFromFile(){
+
+    }
+
+    public void countMaximumAvailableUrinals(String str){
+
+    }
+}
---
+++ Project/src/main/java/org/example/urinals.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class urinals {
+    public static void main(String args[]){
+
+    }
+}
