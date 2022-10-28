debe99aaafc60e23c0e44870e2bbab87f01b699d
mpate133 <mpate133@asu.edu>
2022-10-27 20:34:17 -0700

    false test case added for counting emty urinals

--- Project/src/main/java/org/example/operationsForUrinals.java
+++ Project/src/main/java/org/example/operationsForUrinals.java
@@ -16,7 +16,7 @@ public class operationsForUrinals {
             System.out.println("Enter  the string : ");
             String str = sc.next();
             if (isValidString(str)) {
-                countMaximumAvailableUrinals(str);
+                System.out.println(countMaximumAvailableUrinals(str));
             } else {
                 System.out.println("-1");
             }
@@ -26,84 +26,65 @@ public class operationsForUrinals {
     }
 
     public static boolean isValidString(String str) {
-        Map<Character, Integer> map = new HashMap<>();
         char[] chars = str.toCharArray();
-        for(char c : chars){
-            if(!map.containsKey(c)){
-                map.put(c,0);
+
+        for(int i=0; i<chars.length-1; i++){
+            if((chars[i] != '1' && chars[i] != '0') || (chars[i] == '1' && chars[i] == chars[i+1])){
+                return false;
             }
-            map.put(c, map.get(c) + 1);
         }
 
-        if(chars.length > 0 && map.size() < 3 && ((map.size() == 1 && (map.containsKey('0') || map.containsKey('1'))) || (map.size() == 2 && map.containsKey('0') && map.containsKey('1'))) ){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return true;
     }
 
     public static void readFromFile() throws IOException {
         File file = new File("src\\main\\resources\\urinal.dat");
         BufferedReader br = new BufferedReader(new FileReader(file));
-
+        int res;
         String st;
         while ((st = br.readLine()) != null){
+            res = -1;
             if (isValidString(st)) {
-                countMaximumAvailableUrinals(st);
-            } else {
-                System.out.println("-1");
+                res = countMaximumAvailableUrinals(st);
             }
+
+            // write in file
         }
     }
 
-    public static void countMaximumAvailableUrinals(String str){
+    public static int countMaximumAvailableUrinals(String str){
         int result = -1;
-        boolean flag = true;
         char[] chars = str.toCharArray();
         int charArrayLength = chars.length;
-        for(int i=0; i<chars.length-1; i++){
-            if((chars[i] != '1' && chars[i] != '0') || (chars[i] == '1' && chars[i] == chars[i+1])){
-                flag = false;
-                break;
-            }
-        }
 
-        if(flag){
-            // code for counting maximun number of urinals
-            int changesValues = 0;
-            for(int i=0; i<charArrayLength; i++){
-                if(i == 0) {
-                    if(charArrayLength== 1){
-                        result = 1 - Character.getNumericValue(chars[0]);
-                        break;
-                    }
-                    else if(chars[i] =='0' && chars[i+1] == '0'){
-                        chars[i] = '1';
-                        changesValues += 1;
-                    }
+        // code for counting maximun number of urinals
+        int changesValues = 0;
+        for(int i=0; i<charArrayLength; i++){
+            if(i == 0) {
+                if(charArrayLength== 1){
+                    result = 1 - Character.getNumericValue(chars[0]);
+                    return result;
                 }
-                else if(i == charArrayLength-1) {
-                    if (chars[i - 1] == '0' && chars[i] == '0') {
-                        chars[i] = '1';
-                        changesValues += 1;
-                    }
-                }
-                else {
-                    if (chars[i - 1] == '0' && chars[i] == '0' && chars[i + 1] == '0') {
-                        chars[i] = '1';
-                        changesValues += 1;
-                    }
+                else if(chars[i] =='0' && chars[i+1] == '0'){
+                    chars[i] = '1';
+                    changesValues += 1;
                 }
             }
-            result = changesValues;
+            else if(i == charArrayLength-1) {
+                if (chars[i - 1] == '0' && chars[i] == '0') {
+                    chars[i] = '1';
+                    changesValues += 1;
+                }
+            }
+            else {
+                if (chars[i - 1] == '0' && chars[i] == '0' && chars[i + 1] == '0') {
+                    chars[i] = '1';
+                    changesValues += 1;
+                }
+            }
         }
+        result = changesValues;
+        return result;
 
-        if(personChoice == 0){
-            System.out.println(result);
-        }
-        else{
-          // write in the file
-        }
     }
 }
--- Project/src/test/java/org/example/urinalsTest.java
+++ Project/src/test/java/org/example/urinalsTest.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,13 +13,18 @@ class urinalsTest {
 
     @Test
     void isValidString(){
-        Assertions.assertEquals(false, operation.isValidString(new String("Manan Patel")));
+        assertEquals(false, operation.isValidString("Manan Patel"));
+        System.out.println("====== Manan Patel == TEST ONE EXECUTED For Validation=======");
+        assertEquals(true, operation.isValidString("100"));
+        assertEquals(false, operation.isValidString("0011"));
+        assertEquals(true, operation.isValidString("1001"));
+        assertEquals(true, operation.isValidString("01010100"));
 
-        System.out.println("====== Manan Patel == TEST ONE EXECUTED =======");
     }
 
     @Test
     void countMaximumAvailableUrinals(){
-
+        assertEquals(2, operation.countMaximumAvailableUrinals("10001"));
+        System.out.println("======= '10001' ===== false case written");
     }
 }
