c2aa45c0c1930a96f9c3cfbd3053bb91b76b53ce
mpate133 <mpate133@asu.edu>
2022-10-27 20:00:14 -0700

    added empty methods in urinalsTest file

--- Project/src/test/java/org/example/urinalsTest.java
+++ Project/src/test/java/org/example/urinalsTest.java
@@ -6,7 +6,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class urinalsTest {
 
+    operationsForUrinals operation = new operationsForUrinals();
     @Test
     void main() {
+    }
+
+    @Test
+    void isValidString(){
+
+    }
+
+    @Test
+    void countMaximumAvailableUrinals(){
+
     }
 }
