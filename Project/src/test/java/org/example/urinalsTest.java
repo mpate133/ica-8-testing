26b50be0b73b579b05310dc2756d1d402f8b5209
mpate133 <mpate133@asu.edu>
2022-10-27 20:42:54 -0700

    added variety of valid test cases

--- Project/src/test/java/org/example/urinalsTest.java
+++ Project/src/test/java/org/example/urinalsTest.java
@@ -19,12 +19,28 @@ class urinalsTest {
         assertEquals(false, operation.isValidString("0011"));
         assertEquals(true, operation.isValidString("1001"));
         assertEquals(true, operation.isValidString("01010100"));
+        assertEquals(false, operation.isValidString("10111"));
+        assertEquals(true, operation.isValidString("01010100"));
+        assertEquals(true, operation.isValidString("00100"));
+        assertEquals(false, operation.isValidString("1111"));
+        assertEquals(true, operation.isValidString("0000"));
+        assertEquals(true, operation.isValidString("01"));
+
 
     }
 
     @Test
     void countMaximumAvailableUrinals(){
         assertEquals(1, operation.countMaximumAvailableUrinals("10001"));
-        System.out.println("======= '10001' ===== false case corrected");
+        System.out.println("======= '10001' ===== Fist test case for counting urinals executed");
+        assertEquals(4, operation.countMaximumAvailableUrinals("1000100001000001"));
+        assertEquals(0, operation.countMaximumAvailableUrinals("010101"));
+        assertEquals(0, operation.countMaximumAvailableUrinals("101001"));
+        assertEquals(2, operation.countMaximumAvailableUrinals("10000001"));
+        assertEquals(3, operation.countMaximumAvailableUrinals("00000"));
+        assertEquals(2, operation.countMaximumAvailableUrinals("00100"));
+        assertEquals(2, operation.countMaximumAvailableUrinals("0000"));
+        assertEquals(0, operation.countMaximumAvailableUrinals("01"));
+
     }
 }
